dc9dd0c5c1d0ec2621a66cddb4306fa651084957
Gabriel <107438394+gabrielfinazzo@users.noreply.github.com>
2022-12-10 12:36:13 -0300

    Atualização menus

--- Treino/navbar_responsivo/navbar2/script.js
+++ Treino/navbar_responsivo/navbar2/script.js
@@ -1,8 +1,18 @@
-function abrirMenu() {
+/*function abrirMenu() {
     if (newbox.style.display == "block") {
         newbox.style.display = "none"
     } else {
         newbox.style.display = "block"
     }
         
+}
+*/
+
+function abrirMenu() {
+    let menuMobile = document.querySelector('.boxmenu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+    } else {
+        menuMobile.classList.add('open');
+    }
 }
