4ebd2e3449ab3f9fd88a4b26f56b2c0e0366a3fd
Gabriel <107438394+gabrielfinazzo@users.noreply.github.com>
2022-12-20 15:55:18 -0300

    Projeto Engenharia

---
+++ Projeto_Engenharia/script.js
@@ -0,0 +1,10 @@
+function checkActive() {
+    document.getElementById = ("check")
+        check.style.opacity = "1"
+    
+}
+
+function checkDesactive() {
+    document.getElementById = ("check")
+        check.style.opacity = "0"
+}
