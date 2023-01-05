38475863fe3e3e3bc0528e4d86cba1a5a4a8a898
Gabriel <107438394+gabrielfinazzo@users.noreply.github.com>
2023-01-05 15:10:23 -0300

    Atualização Carrossel

--- projetoeng/script.js
+++ projetoeng/script.js
@@ -20,3 +20,26 @@ function menuShow() {
         
     
 }
+
+
+//Carrossel
+
+let slides = document.querySelectorAll('.slide-container');
+let index = 0;
+
+// next function
+function next() {
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+// previous function
+function prev() {
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
+
+//autoplay carrossel
+setInterval(next, 7000);
