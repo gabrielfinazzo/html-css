4a392bef4a8818fc3d1d94158d1a3499bfea78d4
Gabriel <107438394+gabrielfinazzo@users.noreply.github.com>
2023-02-25 21:43:29 -0300

    Atualização

--- Projetos/projeto_ecommerce/js/main.js
+++ Projetos/projeto_ecommerce/js/main.js
@@ -18,6 +18,15 @@ $(document).ready(function() {
         }
     }); 
 
+    $('.home-slider').owlCarousel({
+        items:1,
+        nav:true,
+        dots:false,
+        autoplay:true,
+        autoplayTimeout:8000,
+        loop:true
+    });
+
 
 });
 
